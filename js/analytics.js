842f53dd37f66167b5c4fc071b626d32e5282791
Akshat Mittal <itsreallyakshat@gmail.com>
2014-04-20 17:35:42 +0530

    Analytics+

---
+++ js/analytics.js
@@ -0,0 +1,12 @@
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50190232-1', 'akshatmittal.github.io');
+  ga('send', 'pageview');
+
+$("a").on('click', function(){ga('send', 'event', 'Tag click: a', 'click');}); // For nav analysis
+$("button").on('click', function(){ga('send', 'event', 'Tag click: button', 'click');}); // For nav analysis
+$(document).on('click', function() {ga('send', 'event', 'Page click', 'click');}); // Heat analysis
+$("img").on('click', function(){ga('send', 'event', 'Tag click: img', 'click');}); // Img analysis
