858f2a1453a1ec0068e2853b3ce22a662535cbf7
Akshat Mittal <itsreallyakshat@gmail.com>
2015-09-19 15:47:36 +0530

    HTTPS fix + improvements

--- js/analytics.js
+++ js/analytics.js
@@ -1,17 +1,18 @@
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
   ga('create', 'UA-50190232-1', 'akshatmittal.github.io');
   ga('require', 'displayfeatures');
   ga('send', 'pageview');
-
-$("a").on('click', function(){ga('send', 'event', 'Tag click: a', 'click');}); // For nav analysis
-$("button").on('click', function(){ga('send', 'event', 'Tag click: button', 'click');}); // For nav analysis
-$(document).on('click', function() {ga('send', 'event', 'Page click', 'click');}); // Heat analysis
-$("img").on('click', function(){ga('send', 'event', 'Tag click: img', 'click');}); // Img analysis
-window.onresize = function() {ga('send', 'event', 'Window Resized', 'click');} // Why will they even resize the window?
-window.onload = function() {
-	if (self != top) top.location = self.location;
-}
+  window.onload = function() {
+    if (self != top) top.location = self.location;
+  }
