7a7b21097b248fea319b39f12f3101e97900489b
Akshat Mittal <itsreallyakshat@gmail.com>
2015-09-26 00:24:09 +0530

    HTTPS All Page Fix

--- js/analytics.js
+++ js/analytics.js
@@ -1,18 +1,21 @@
-  (function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-      (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+if ('protocol' in location) {
+  if (location.protocol == "http:") location.protocol = "https:";
+}
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-  ga('create', 'UA-50190232-1', 'akshatmittal.github.io');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
-  window.onload = function() {
-    if (self != top) top.location = self.location;
-  }
+ga('create', 'UA-50190232-1', 'akshatmittal.github.io');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
+window.onload = function() {
+  if (self != top) top.location = self.location;
+}
